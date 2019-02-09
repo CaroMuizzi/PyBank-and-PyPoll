e250f2c9c4b5ba6f04d3cd23e1a5e8ea48db75a3
cazzica <cmuizzi@gmail.com>
2019-02-09 15:13:20 -0500

    amazing4

--- PyPol/main.py
+++ PyPol/main.py
@@ -2,6 +2,15 @@ import os
 import csv
 import sys
 
+#In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
+#You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+# * The total number of votes cast
+# * A complete list of candidates who received votes
+# * The percentage of votes each candidate won
+# * The total number of votes each candidate won
+# * The winner of the election based on popular vote.
+
 # Path to collect data from the Resources folder
 csvpath = os.path.join("Resources", "election_data.csv")
 
@@ -13,7 +22,7 @@ Candidates_list=[]
 Count_votes_per_candidate=[]
 Total_candidates_list=[]
 Unique_candidates_list=[]
-#Votes_list=[]
+
 
 # Read using CSV module
 with open(csvpath, newline='', encoding="utf8") as election_data:
@@ -88,3 +97,5 @@ for key, value in summary_total_candidates.items():
    print(f"{key}: {round(((int(value)/int(length))*100),1)}% ({value})",file = sys.stderr)
 print("-----------------------",file = sys.stderr)
 print(f"Winner: {winner}",file = sys.stderr)
+
+# Carolina Muizzi
